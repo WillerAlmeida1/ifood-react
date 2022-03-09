f230ad34d689c239b7faa7402a00d75a6ba46848
Willer Almeida Silva <contatowilleralmeida@gmail.com>
2022-03-09 19:23:21 -0300

    projeto ifood

---
+++ src/AppEntry.js
@@ -0,0 +1,6 @@
+import 'react-native-gesture-handler';
+import registerRootComponent from 'expo/build/launch/registerRootComponent';
+
+import Routes from './routes'
+
+registerRootComponent(Routes);
---
+++ src/pages/Perfil/index.js
@@ -0,0 +1,18 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { SafeAreaView, Image, View, ButtonContainer } from './style';
+import { Text } from 'react-native';
+
+import banner from '../../assets/img/banner.png';
+import Button from '../../components/Button';
+
+export default function Principal() {
+  return (
+    <>
+      <StatusBar style='theme-dark'/>
+       <SafeAreaView>
+          
+       </SafeAreaView>
+    </>
+  );
+}
